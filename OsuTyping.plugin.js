f0375fbc1cebb2fb97112434d8abacf1f68f2fab
happynger <happynger@gmai.com>
2019-08-31 11:15:39 -0700

    Create OsuTyping.plugin.js
    Created the javascript file

---
+++ OsuTyping.plugin.js
@@ -0,0 +1,47 @@
+//META{"name":"OsuTyping"}
+
+class OsuTyping
+{
+	getName() {return "Osu Typing";}
+	getVersion() {return "0.1";}
+	getAuthor() {return "Happy Ori";}
+	getDescription() {return "Adds a clicking sound when you type";}
+
+	initConstructor() {}
+
+	load() {}
+
+	start()
+	{
+
+	}
+
+	initialize()
+	{
+		this.onChatInput() = e =>
+		{
+			const chatbox = e.target;
+		}
+	}
+
+	stop()
+	{
+		const chatbox = this.getChat();
+		if (chatbox)
+			chatbox.removeEventListener("keydown", this.onChatInput);
+	}
+
+	onSwitch()
+	{
+		const chatbox = this.getChat();
+		if (chatbox)
+			chatbox.addEventListener("keydown", this.onChatInput);
+	}
+
+	getChat()
+	{
+		let chatbox = document.getElementsByClassName("chat-3bRxxu")[0];
+
+		return chatbox ? chatbox.getElementsByTagName("textarea")[0] : null;
+	}
+}
